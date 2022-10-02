e6e2fc54f239a7fb7b09d95d6d68a5c5e820dc4a
Harshit Choudhary <harshit1840077@gmail.com>
2022-10-02 23:33:13 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-console.log("Hello From app.js")
+ import axios from 'axios'
+ 
+
+ let cartCounter = document.querySelector('#cartCounter')
+ let addToCart = document.querySelectorAll('.add-to-cart')
+
+ function updateCart(pizza) {
+        axios.post('/update-cart',pizza).then(res=>{
+            cartCounter.innerText = res.data.totalQty
+            console.log(res);
+        })  
+ }
+ 
+ addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
+    })
+})
--- server.js
+++ server.js
@@ -1,10 +1,58 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
+const mongoose = require('mongoose')
+const session = require('express-session')
 const expressLayout = require('express-ejs-layouts')
+const flash = require('express-flash')
+const { json } = require('express')
+const MongoDbStore = require('connect-mongo')(session)
 const PORT = process.env.PORT || 3000
 app.use(express.static('public'))
+app.use(express.json())
+
+ 
+// Database connection
+const url='mongodb://localhost/Pizza'
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+const connections = mongoose.connection;
+mongoose.connection
+    .once('open', function () {
+      console.log('MongoDB running');
+    })
+    .on('error', function (err) {
+      console.log(err);
+    });
+
+
+
+// Session store
+let mongoStore = new MongoDbStore({
+  mongooseConnection: mongoose.connection,
+  collection: 'sessions'
+})
+
+
+// Session config
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store:mongoStore,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hour
+}))
+
+app.use(flash())
+
+// Global middleware
+app.use((req, res, next) => {
+  res.locals.session = req.session
+  res.locals.user = req.user
+  next()
+})
+
 
 
 
@@ -13,21 +61,10 @@ app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
 
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
+require('./routes/web')(app)
 
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
 
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
 
 app.listen(PORT , ()=>{
     console.log(`listening on port ${PORT}`)
